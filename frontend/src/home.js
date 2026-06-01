da561ff25010da2e3e101f95c1ace9de972b2a53
Vicentmeatheus15 <vicentematheus1507@gmail.com>
2026-06-01 20:59:26 -0300

    feat: implement login button functionality to show popup

--- frontend/src/home.js
+++ frontend/src/home.js
@@ -1,10 +1,14 @@
 import './home.css';
+import {useState} from "react"
 
 function HomePage(){
+
+    const [showPopUp, setShowPopUp] = useState(false)
+
     return(
         <div className= "grid-container">
             <header className="header">
-                <button className="logButton" onClick={() =>{}} >Login</button>
+                <button className="logButton" onClick={() =>{setShowPopUp(true)}} >Login</button>
             </header>
             <aside className="sidebar">Menu Lateral</aside>
             <main>
